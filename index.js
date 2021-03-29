81c00ab40ed473a4218b711c724529078d9348ce
friedrickshrimp <77844844+friedrickshrimp@users.noreply.github.com>
2021-03-29 11:07:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,83 @@ const paceID = process.env.PACE_ID;
 client.login(process.env.DJS_TOKEN);
 
 // When there is a message sent...
-client.on('message', msg=> {
-    // See if the message includes <@!PACE_ID> because that's how tags work apparently
-    if(msg.content.includes("<@!" + paceID + ">")) {
-        msg.channel.send("Yo Pace!");
-}
+client.on('message', async (msg) => {
+    if (msg.channel.name != "worship-music"
+        && msg.channel.name != "daily-encouragement"
+        && msg.channel.name != "accountability"
+        && msg.channel.name != "prayer-requests"
+        && msg.channel.name != "requesting-help"
+        && msg.channel.name != "theology"
+        && msg.channel.name != "testimonies") {
+        var date = new Date();
+
+        // See if the message includes <@!PACE_ID> because that's how tags work apparently
+
+        if (msg.content.includes("<@!" + paceID + ">") && date.getMonth() == 3 && date.getDate() == 1) {
+
+            if (msg.content.toLowerCase().includes("yo pace") || msg.content.toLowerCase().includes("yo, pace") || msg.content.toLowerCase().includes("yo! pace") || msg.content.toLowerCase().includes("yopace")) {
+                const g = "🇬";
+                const o = "🅾️";
+                const o2 = "🇴";
+                const d = "🇩";
+                const j = "🇯";
+                const o3 = "⭕";
+                const b = "🇧";
+
+                msg.react(g);
+                msg.react(o);
+                msg.react(o2);
+                msg.react(d);
+                msg.react(j);
+                msg.react(o3);
+                msg.react(b);
+            }
+
+            else {
+                const u = "🇺";
+                const d = "🇩"
+                const i = "🇮";
+                const yopace_d = client.emojis.cache.get("826088244818346032");
+                const n = "🇳";
+                const t = "🇹";
+                const s = "🇸";
+                const a = "🇦";
+                const y = "🇾";
+                const yopace_y = client.emojis.cache.get("826088244193656902");
+                const o = "🇴";
+                const p = "🇵";
+                const a2 = "🅰️";
+                const c = "🇨";
+                const e = "🇪";
+                const alarm = client.emojis.cache.get("826095842431205437")
+
+
+                msg.react(u);
+                msg.react(d);
+                msg.react(i);
+                msg.react(yopace_d);
+                msg.react(n);
+                msg.react(t);
+                msg.react(s);
+                msg.react(a);
+                msg.react(y);
+                msg.react(yopace_y);
+                msg.react(o);
+                msg.react(p);
+                msg.react(a2);
+                msg.react(c);
+                msg.react(e);
+                msg.react(alarm).then((reaction) => {
+                    msg.channel.send("Yo Pace!");
+                });
+
+            }
+
+
+        }
+        else if(msg.content.includes("<@!" + paceID + ">")){
+            msg.channel.send("Yo Pace!");
+        }
+
+    }
 });
